87ec5166abc6fe60408a9e26f87d913b3bd2e719
blt1 <blt1@rice.edu>
2017-08-03 14:26:49 -0500

    updated seal module with omegaselector

--- XiAnalyzer/plugins/SealModule.cc
+++ XiAnalyzer/plugins/SealModule.cc
@@ -2,14 +2,16 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "XiAnalyzer/XiAnalyzer/interface/XiSelector.h"
 #include "XiAnalyzer/XiAnalyzer/interface/V0Selector.h"
+#include "XiAnalyzer/XiAnalyzer/interface/OmegaSelector.h"
 #include "XiAnalyzer/XiAnalyzer/interface/XiCorrelation.h"
 #include "XiAnalyzer/XiAnalyzer/interface/V0Correlation.h"
 #include "XiAnalyzer/XiAnalyzer/interface/XiMassPt.h"
 #include "XiAnalyzer/XiAnalyzer/interface/XiOmTTree.h"
 
-DEFINE_FWK_MODULE( XiSelector );
-DEFINE_FWK_MODULE( V0Selector );
-DEFINE_FWK_MODULE( XiCorrelation );
-DEFINE_FWK_MODULE( V0Correlation );
-DEFINE_FWK_MODULE( XiMassPt );
-DEFINE_FWK_MODULE( XiOmTTree );
+DEFINE_FWK_MODULE(XiSelector);
+DEFINE_FWK_MODULE(V0Selector);
+DEFINE_FWK_MODULE(OmegaSelector);
+DEFINE_FWK_MODULE(XiCorrelation);
+DEFINE_FWK_MODULE(V0Correlation);
+DEFINE_FWK_MODULE(XiMassPt);
+DEFINE_FWK_MODULE(XiOmTTree);
