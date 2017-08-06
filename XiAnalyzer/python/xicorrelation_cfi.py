a2b65f353c5c47d04618292fb5756289cf7ca7b6
Benjamin Tran <blt1@rice.edu>
2017-08-06 18:41:12 -0500

    Clarified pt Binning in xicorrelation config

--- XiAnalyzer/python/xicorrelation_cfi.py
+++ XiAnalyzer/python/xicorrelation_cfi.py
@@ -16,26 +16,23 @@ hltHM.andOr = cms.bool(True)
 hltHM.throw = cms.bool(False)
 
 xiCorrelation          = cms.EDAnalyzer('XiCorrelation',
-        trkSrc         = cms.InputTag('generalTracks'),
-        xiCollection   = cms.InputTag('selectV0CandidatesLowXi:Xi'),
-        vertexCollName = cms.InputTag('offlinePrimaryVertices'),
-        zVtxHigh       = cms.double(15.0),
-        zVtxLow        = cms.double(-15.0),
-        ptMax_ass      = cms.double(3.0),
-        ptMin_ass      = cms.double(0.3),
+        bkgnum         = cms.double(20.0),
         etaMax_ass     = cms.double(2.4),
         etaMin_ass     = cms.double(-2.4),
         multHigh       = cms.int32(220),
         multLow        = cms.int32(185),
-        bkgnum         = cms.double(20.0),
+        peakFactor     = cms.int32(2),
         ptBin          = cms.vdouble(1.0, 1.4, 1.8, 2.2, 2.8, 3.6, 4.6, 6.0),
-        # Root fit values
-        #xiMassMean     = cms.vdouble(1.32245, 1.3226, 1.32259, 1.32247, 1.32239, 1.32224, 1.32212),
-        #xiMassSigma    = cms.vdouble(0.00344097, 0.00544116, 0.00424282, 0.00539586, 0.00588911, 0.00537808, 0.00281928),
-        # RooFit values
+        ptMax_ass      = cms.double(3.0),
+        ptMin_ass      = cms.double(0.3),
+        sideFactor     = cms.int32(3),
+        trkSrc         = cms.InputTag('generalTracks'),
+        vertexCollName = cms.InputTag('offlinePrimaryVertices'),
+        xiCollection   = cms.InputTag('selectV0CandidatesLowXi:Xi'),
+        zVtxHigh       = cms.double(15.0),
+        zVtxLow        = cms.double(-15.0),
+        # RooFit values for pT bins 1,1.4,1.8,2.2,2.8,3.6,4.6,6
+        PtBinNum       = cms.int32(7)
         xiMassMean     = cms.vdouble(1.32296, 1.32254, 1.3225, 1.32246, 1.32242, 1.32226, 1.32214),
         xiMassSigma    = cms.vdouble(0.00558245, 0.00520491, 0.00518877, 0.00514213, 0.00512142, 0.00512142, 0.00490732),
-        peakFactor     = cms.int32(2),
-        sideFactor     = cms.int32(3),
-        PtBinNum       = cms.int32(7)
         )
