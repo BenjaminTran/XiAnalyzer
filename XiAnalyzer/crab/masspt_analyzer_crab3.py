338b9a014fa97c75b769d5d6e5f001e5e8e2f465
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-07 06:21:48 +0200

    Initial commit

---
+++ XiAnalyzer/crab/masspt_analyzer_crab3.py
@@ -0,0 +1,38 @@
+import os
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'HLT185_220FlowKslav2ppb2016PD5CorrelationJL9'
+config.General.workArea = 'HLT185_220FlowKslav2ppb2016PD1'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+#config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = os.path.expandvars('/afs/cern.ch/user/b/btran/work/CMSSW_8_0_24/src/XiAnalyzer/XiAnalyzer/test/v0analysis_cfg.py')
+
+config.section_("Data")
+config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.userInputFiles = list(open('HMMC90.txt'))
+config.Data.inputDBS = 'phys03'
+#config.Data.primaryDataset = 'MinBias_TuneZ2star_7TeV_pythia6'
+#config.Data.splitting = 'FileBased'
+#config.Data.unitsPerJob = 1
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 60000
+#config.Data.totalUnits = 300
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/btran/'
+config.Data.useParent = True
+config.Data.publication = False
+config.Data.publishDBS = 'phys03'
+config.Data.outputDatasetTag = 'pPb2016_V0_Rereco_HM185_220Flow'
+
+config.section_("Site")
+config.Site.storageSite = 'T2_CH_CERN'
+config.Site.whitelist = ['T2_US_MIT']
+
