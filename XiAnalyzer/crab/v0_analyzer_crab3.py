9a91875fe626a00dd758e17222753a1844c145c9
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-07 04:50:05 +0200

    removed .pyc cached, changed output dataset tag

--- XiAnalyzer/crab/v0_analyzer_crab3.py
+++ XiAnalyzer/crab/v0_analyzer_crab3.py
@@ -12,11 +12,11 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = os.path.expandvars('/afs/cern.ch/user/b/btran/work/CMSSW_8_0_24/src/XiAnalyzer/XiAnalyzer/test/v0analysis_cfg.py')
 
 config.section_("Data")
-#config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.userInputFiles = list(open('HMMC90.txt'))
 config.Data.inputDBS = 'phys03'
@@ -30,7 +30,7 @@ config.Data.outLFNDirBase = '/store/group/phys_heavyions/btran/'
 config.Data.useParent = True
 config.Data.publication = False
 config.Data.publishDBS = 'phys03'
-config.Data.outputDatasetTag = 'pPb2016_Cascade_Rereco_HM185_220Flow'
+config.Data.outputDatasetTag = 'pPb2016_V0_Rereco_HM185_220Flow'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
