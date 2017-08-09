0a9dcebbd925f30adea983ea094c0daa3acb8c6f
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-09 05:44:05 +0200

    crabcfg JL11-16

--- XiAnalyzer/crab/masspt_analyzer_crab3.py
+++ XiAnalyzer/crab/masspt_analyzer_crab3.py
@@ -3,8 +3,8 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'HLT185_220FlowMassPtv2ppb2016PD6CorrelationJL10'
-config.General.workArea = 'HLT185_220FlowMassPtv2ppb2016PD6'
+config.General.requestName = 'HLT185_220FlowMassPtv2ppb2016PD1CorrelationJL16'
+config.General.workArea = 'HLT185_220FlowMassPtv2ppb2016PD1'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
@@ -12,12 +12,12 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = os.path.expandvars('/afs/cern.ch/user/b/btran/work/CMSSW_8_0_24/src/XiAnalyzer/XiAnalyzer/test/masspt_cfg.py')
 
 config.section_("Data")
-#config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.userInputFiles = list(open('HMMC90.txt'))
 config.Data.inputDBS = 'phys03'
 #config.Data.primaryDataset = 'MinBias_TuneZ2star_7TeV_pythia6'
