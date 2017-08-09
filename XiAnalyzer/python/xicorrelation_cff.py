3135082f90da940ba588e2d5b0c3721cb7e1a6c0
Benjamin Tran <blt1@rice.edu>
2017-08-09 15:58:53 -0500

    added rapidity module

--- XiAnalyzer/python/xicorrelation_cff.py
+++ XiAnalyzer/python/xicorrelation_cff.py
@@ -6,5 +6,10 @@ from XiAnalyzer.XiAnalyzer.xicorrelation_cfi import *
 
 
 
-xiCorrelator = xiCorrelation.clone();
-hltHM = hltHM.clone();
+xiCorrelationRapidity = xiCorrelation.clone(
+        ptBin         = cms.vdouble(1.0, 1.4, 1.8, 2.2, 2.8, 3.6, 4.6, 6.0, 7.2, 8.5, 10, 20),
+        PtBinNum      = cms.int32(11),
+        xiMassMean    = cms.vdouble(1.32338 ,1.3227 ,1.32243 ,1.32235 ,1.32224 ,1.32218 ,1.32214 ,1.32211 ,1.32207 ,1.32208 ,1.32236),
+        xiMassSigma   = cms.vdouble(0.00559704, 0.00538643, 0.00517754, 0.00468905, 0.00448518, 0.00413725, 0.00434247, 0.00382751, 0.00463059, 0.00316714, 0.00352118)
+        )
+hltHM = hltHM.clone()
