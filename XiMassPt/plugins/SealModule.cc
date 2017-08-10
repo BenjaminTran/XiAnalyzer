0a5f2efb6fe9c1fc526625549c71850d23c0a36e
Benjamin Tran <blt1@rice.edu>
2017-08-10 10:57:25 -0500

    removed deprecated XiMassPt dir

--- XiMassPt/plugins/SealModule.cc
+++
@@ -1,6 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "XiAnalyzer/XiMassPt/interface/XiMassPt.h"
-
-DEFINE_FWK_MODULE(XiMassPt);
-
--- XiMassPt/python/ximasspt_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from XiAnalyzer.XiMassPt.ximasspt_cfi import *
-
-xiMassPt = xiMassPt.clone();
-#hltHM = hltHM.clone();
