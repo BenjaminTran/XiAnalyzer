610a1ac63beec28d5e1b6de0d143fda8242f71b6
Benjamin Tran <blt1@rice.edu>
2017-08-11 10:00:53 -0500

    Added reverse PDs

--- XiAnalyzer/crab/v0_analyzer_crab3.py
+++ XiAnalyzer/crab/v0_analyzer_crab3.py
@@ -3,8 +3,10 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'HLT185_250FlowV0v2ppb2016PD6CorrelationJL5'
-config.General.workArea = 'HLT185_250FlowV0v2ppb2016PD6Rap'
+#config.General.requestName = 'HLT185_250FlowV0v2ppb2016PD1CorrelationJL6'
+#config.General.workArea = 'HLT185_250FlowV0v2ppb2016PD1Rap'
+config.General.requestName = 'HLT185_250FlowV0v2pbp2016PD1CorrelationJL6'
+config.General.workArea = 'HLT185_250FlowV0v2pbp2016PD1Rap'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
@@ -17,7 +19,14 @@ config.section_("Data")
 #config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#
+config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.userInputFiles = list(open('HMMC90.txt'))
 config.Data.inputDBS = 'phys03'
 #config.Data.primaryDataset = 'MinBias_TuneZ2star_7TeV_pythia6'
@@ -30,7 +39,7 @@ config.Data.outLFNDirBase = '/store/group/phys_heavyions/btran/'
 config.Data.useParent = True
 config.Data.publication = False
 config.Data.publishDBS = 'phys03'
-config.Data.outputDatasetTag = 'pPb2016_V0_Rereco_HM185_250Flow'
+config.Data.outputDatasetTag = 'Pbp2016_V0_Rereco_HM185_250Flow'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
