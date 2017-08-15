9ecce6c3490619b71e93a2bfd341bef44630b4c1
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-15 18:37:04 +0200

    submitting jobs

--- XiAnalyzer/crab/v0_analyzer_crab3.py
+++ XiAnalyzer/crab/v0_analyzer_crab3.py
@@ -3,10 +3,10 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-#config.General.requestName = 'HLT185_250FlowV0v2ppb2016PD1CorrelationJL6'
-#config.General.workArea = 'HLT185_250FlowV0v2ppb2016PD1Rap'
-config.General.requestName = 'HLT185_250FlowV0v2pbp2016PD3CorrelationJL8'
-config.General.workArea = 'HLT185_250FlowV0v2pbp2016PD3Rap'
+#config.General.requestName = 'HLT185_250FlowV0v2ppb2016PD4CorrelationJL9'
+#config.General.workArea = 'HLT185_250FlowV0v2ppb2016PD4Rap'
+config.General.requestName = 'HLT185_250FlowV0v2pbp2016PD6CorrelationJL18'
+config.General.workArea = 'HLT185_250FlowV0v2pbp2016PD6Rap'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
@@ -23,17 +23,17 @@ config.section_("Data")
 #
 #config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
 #config.Data.userInputFiles = list(open('HMMC90.txt'))
 config.Data.inputDBS = 'phys03'
 #config.Data.primaryDataset = 'MinBias_TuneZ2star_7TeV_pythia6'
 #config.Data.splitting = 'FileBased'
 #config.Data.unitsPerJob = 1
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 50000
+config.Data.unitsPerJob = 60000
 #config.Data.totalUnits = 300
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/btran/'
 config.Data.useParent = True
