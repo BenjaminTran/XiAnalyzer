1792997d69443041f97a024c3651356d613f3318
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-17 19:24:42 +0200

    Working shared xi header

--- XiAnalyzer/crab/crab3_xi_analyzer.py
+++ XiAnalyzer/crab/crab3_xi_analyzer.py
@@ -1,4 +1,4 @@
-import xianalyzerheader.py
+import xianalyzerheader as xi
 if __name__ == '__main__':
 
     from CRABAPI.RawCommand import crabCommand
@@ -20,28 +20,28 @@ if __name__ == '__main__':
         counter = 0
 
     PDcounter = 2
-    #config.Data.outputDatasetTag = 'Pbp2016_Cascade_Rereco_HM185_250Flow'
-    config.Data.outputDatasetTag = 'pPb2016_Cascade_Rereco_HM185_250Flow'
+    xi.config.Data.outputDatasetTag = 'Pbp2016_Cascade_Rereco_HM185_250Flow'
+    #xi.config.Data.outputDatasetTag = 'pPb2016_Cascade_Rereco_HM185_250Flow'
 
-    config.General.requestName = 'HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'CorrelationJL' + str(counter)
-    config.General.workArea = 'crab_dir/HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'Rap'
+    xi.config.General.requestName = 'HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'CorrelationJL' + str(counter)
+    xi.config.General.workArea = 'crab_dir/HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'Rap'
 
-    #config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    xi.config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
     #
-    #config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-    #config.Data.userInputFiles = list(open('HMMC90.txt'))
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #xi.config.Data.userInputFiles = list(open('HMMC90.txt'))
 
     with open( 'XiVarStore.dat', 'w' ) as fle:
         counter = counter + 1
         fle.write( str(counter) )
-    submit(config)
+    submit(xi.config)
