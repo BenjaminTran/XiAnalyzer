afbd0971832ad0b17118f8f4da796142e086d1f4
Benjamin Tran <blt1@rice.edu>
2017-08-17 12:08:11 -0500

    use crabAPI instead of crab submit

--- XiAnalyzer/crab/crab3_xi_analyzer.py
+++ XiAnalyzer/crab/crab3_xi_analyzer.py
@@ -1,32 +1,47 @@
 import xianalyzerheader.py
-try:
-    with open( 'XiVarStore.dat', 'r' ) as fle:
-        counter = int( fle.readline() )
-except FileNotFoundError:
-    counter = 0
+if __name__ == '__main__':
 
-PDcounter = 2
-#config.Data.outputDatasetTag = 'Pbp2016_Cascade_Rereco_HM185_250Flow'
-config.Data.outputDatasetTag = 'pPb2016_Cascade_Rereco_HM185_250Flow'
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
 
-config.General.requestName = 'HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'CorrelationJL' + str(counter)
-config.General.workArea = 'crab_dir/HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'Rap'
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
 
-#config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#
-#config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
-#config.Data.userInputFiles = list(open('HMMC90.txt'))
+    try:
+        with open( 'XiVarStore.dat', 'r' ) as fle:
+            counter = int( fle.readline() )
+    except FileNotFoundError:
+        counter = 0
 
-with open( 'XiVarStore.dat', 'w' ) as fle:
-    counter = counter + 1
-    fle.write( str(counter) )
+    PDcounter = 2
+    #config.Data.outputDatasetTag = 'Pbp2016_Cascade_Rereco_HM185_250Flow'
+    config.Data.outputDatasetTag = 'pPb2016_Cascade_Rereco_HM185_250Flow'
+
+    config.General.requestName = 'HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'CorrelationJL' + str(counter)
+    config.General.workArea = 'crab_dir/HLT185_250FlowCascadev2ppb2016PD' + str(PDcounter) + 'Rap'
+
+    #config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #
+    #config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #config.Data.userInputFiles = list(open('HMMC90.txt'))
+
+    with open( 'XiVarStore.dat', 'w' ) as fle:
+        counter = counter + 1
+        fle.write( str(counter) )
+    submit(config)
