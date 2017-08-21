7e00b577a2eb9476a9e438e3da3fa8d58816abec
Benjamin Tran <blt1@rice.edu>
2017-08-21 16:30:21 -0500

    made loose reco cut options

--- XiAnalyzer/test/v0analysis_cfg.py
+++ XiAnalyzer/test/v0analysis_cfg.py
@@ -87,6 +87,8 @@ process.TFileService = cms.Service("TFileService",
 #process.XiAnalysis = cms.Sequence(process.hltHM*process.selectV0CandidatesLowXi*process.xiCorrelation)
 process.V0CorrAnalysis = cms.Sequence(process.selectV0CandidatesNewlambda*process.selectV0CandidatesNewkshort*process.v0Correlation)
 process.V0CorrAnalysisRapidity = cms.Sequence(process.selectV0CandidatesNewlambdaRapidity*process.selectV0CandidatesNewkshortRapidity*process.v0CorrelationRapidity)
+process.V0CorrAnalysisRapidityLoose = cms.Sequence(process.selectV0CandidatesNewlambdalooseRapidity*process.selectV0CandidatesNewkshortlooseRapidity*process.v0CorrelationRapidity)
+process.V0CorrAnalysisRapidityTight = cms.Sequence(process.selectV0CandidatesNewlambdatightRapidity*process.selectV0CandidatesNewkshorttightRapidity*process.v0CorrelationRapidity)
 
 process.p = cms.Path(process.V0CorrAnalysisRapidity)
 
