31b8e7f37002312b24c154c238f25a2184166bbb
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-22 00:17:37 +0200

    submitted jobs

--- XiAnalyzer/python/v0correlation_cff.py
+++ XiAnalyzer/python/v0correlation_cff.py
@@ -13,4 +13,14 @@ v0CorrelationRapidity = v0Correlation.clone(
         ptcut_ks       = cms.untracked.vdouble(0.2,0.4,0.6,0.8,1.0,1.4,1.8,2.2,2.8,3.6,4.6,6.0,7.0,8.5,10.0,15.0,20.0,25.0,30.0),
         ptcut_la       = cms.untracked.vdouble(0.8,1.0,1.4,1.8,2.2,2.8,3.6,4.6,6.0,7.0,8.5,10.0,15.0,20.0,25.0,30.0)
         )
+
+v0CorrelationlooseRapidity = v0CorrelationRapidity.clone(
+        laCollection   = cms.untracked.InputTag('selectV0CandidatesNewlambdalooseRapidity:Lambda'),
+        ksCollection   = cms.untracked.InputTag('selectV0CandidatesNewkshortlooseRapidity:Kshort'),
+        )
+
+v0CorrelationtightRapidity = v0CorrelationRapidity.clone(
+        laCollection   = cms.untracked.InputTag('selectV0CandidatesNewlambdatightRapidity:Lambda'),
+        ksCollection   = cms.untracked.InputTag('selectV0CandidatesNewkshorttightRapidity:Kshort'),
+        )
 #hltHM = hltHM.clone()
