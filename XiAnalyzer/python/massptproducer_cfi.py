fbd43979acfa257929a7e96ff6b7658e228d7ada
Benjamin Tran <blt1@rice.edu>
2017-08-21 23:13:39 -0500

    Set for Omega testing

--- XiAnalyzer/python/massptproducer_cfi.py
+++ XiAnalyzer/python/massptproducer_cfi.py
@@ -6,6 +6,7 @@ MassPt = cms.EDAnalyzer('MassPtProducer',
         trkSrc         = cms.InputTag('generalTracks'),
         vertexCollName = cms.InputTag('offlinePrimaryVertices'),
         xiCollection   = cms.InputTag('selectV0CandidatesLowXi:Xi'),
+        omCollection   = cms.InputTag('selectOmCandidatesNew:Omega'),
         multHigh       = cms.double(220),
         multLow        = cms.double(185),
         zVtxHigh       = cms.double(15.0),
@@ -15,5 +16,6 @@ MassPt = cms.EDAnalyzer('MassPtProducer',
         MC             = cms.untracked.bool(False),
         ks             = cms.untracked.bool(True),
         la             = cms.untracked.bool(True),
-        xi             = cms.untracked.bool(True)
+        xi             = cms.untracked.bool(True),
+        om             = cms.untracked.bool(True)
 )
