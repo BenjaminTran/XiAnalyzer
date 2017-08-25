d5dbfaf1be40b3295859bb2622cab356293d4d00
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-25 02:46:30 +0200

    job submission

--- XiAnalyzer/crab/v0_analyzer_crab3.py
+++ XiAnalyzer/crab/v0_analyzer_crab3.py
@@ -1,13 +1,18 @@
 import v0analyzerheader as v0
 
-#To submit edit counter, CollID, inputDataset
+#To submit edit PDcounter, CollID, inputDataset
+try:
+    with open( 'V0VarStore.dat', 'r' ) as fle:
+        counter = int( fle.readline() )
+except FileNotFoundError:
+    counter = 0
 
-PDcounter = 1
-collID = 'ppb'
-#collID = 'pbp'
+PDcounter = 2
+#collID = 'ppb'
+collID = 'pbp'
 
-v0.config.General.workArea = 'crab_dir/HLT185_250FlowV0v2' + collID + '2016PD' + str(PDcounter) + 'Rap'
-v0.config.General.requestName = 'HLT185_250FlowV0v2' + collID + '2016PD' + str(PDcounter) + 'CorrelationJL' + str(counter)
+v0.config.General.workArea = 'crab_dir/HLT185_250FlowV0v2' + collID + '2016PD' + str(PDcounter) + 'RapMC'
+v0.config.General.requestName = 'HLT185_250FlowV0v2' + collID + '2016PD' + str(PDcounter) + 'CorrelationMCJL' + str(counter)
 
 if __name__ == '__main__':
 
@@ -23,13 +28,8 @@ if __name__ == '__main__':
         except ClientException as cle:
             print "Failed submitting task: %s" % (cle)
 
-    try:
-        with open( 'V0VarStore.dat', 'r' ) as fle:
-            counter = int( fle.readline() )
-    except FileNotFoundError:
-        counter = 0
 
-    v0.config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+    #v0.config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
     #v0.config.Data.inputDataset = '/PAHighMultiplicity2/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
     #v0.config.Data.inputDataset = '/PAHighMultiplicity3/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
     #v0.config.Data.inputDataset = '/PAHighMultiplicity4/davidlw-RecoSkim2016_pPb_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
@@ -43,6 +43,9 @@ if __name__ == '__main__':
     #v0.config.Data.inputDataset = '/PAHighMultiplicity5/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
     #v0.config.Data.inputDataset = '/PAHighMultiplicity6/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
     #v0.config.Data.userInputFiles = list(open('HMMC90.txt'))
+    # MC
+    #v0.config.Data.inputDataset = '/ReggeGribovPartonMC_EposLHC_pPb_4080_4080_DataBS/davidlw-RecoSkim2016_pPb_V0_v1-2fc6918bc3c19ca88eae36cad5440243/USER'
+    v0.config.Data.inputDataset = '/ReggeGribovPartonMC_EposLHC_PbP_4080_4080_DataBS/davidlw-RecoSkim2016_Pbp_V0_v1-2fc6918bc3c19ca88eae36cad5440243/USER'
 
     with open( 'V0VarStore.dat', 'w' ) as fle:
         counter = counter + 1
