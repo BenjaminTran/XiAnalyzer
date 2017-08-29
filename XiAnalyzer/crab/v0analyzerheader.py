eb27011ccd3d76a7c55c2f8ddb9d74e066e2fea1
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-08-29 03:55:45 +0200

    job submission

--- XiAnalyzer/crab/v0analyzerheader.py
+++ XiAnalyzer/crab/v0analyzerheader.py
@@ -26,5 +26,5 @@ config.Data.outputDatasetTag = 'RapidityCut_V0_Rereco_HM185_250Flow'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
-#config.Site.whitelist = ['T2_US_MIT','T2_US_Vanderbilt']
-config.Site.whitelist = ['T2_US_Vanderbilt']
+config.Site.whitelist = ['T2_US_MIT']
+#config.Site.whitelist = ['T2_US_Vanderbilt']
