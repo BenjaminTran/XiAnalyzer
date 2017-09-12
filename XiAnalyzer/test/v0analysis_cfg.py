baf0637ef0e070c39abfed5f81a7ccc55312b3ef
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-09-12 05:15:36 +0200

    Job submission

--- XiAnalyzer/test/v0analysis_cfg.py
+++ XiAnalyzer/test/v0analysis_cfg.py
@@ -38,7 +38,7 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(5000)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
