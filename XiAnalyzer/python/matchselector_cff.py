5e9b44691c65b33436c9ed0c9bac516ada9d52bf
Benjamin Tran <blt1@rice.edu>
2017-09-20 02:52:11 -0500

    updated modules

--- XiAnalyzer/python/matchselector_cff.py
+++ XiAnalyzer/python/matchselector_cff.py
@@ -1,3 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 from XiAnalyzer.XiAnalyzer.v0selector_cfi import *
+
+MatchCandidatesKshort = MatchCandidates.clone()
+
+MatchCandidatesLambda = MatchCandidates.clone(
+        v0IDName = cms.string('Lambda'),
+        gnV0Collection = cms.InputTag('selectGenCandidatesLambda:Lambda')
+        )
