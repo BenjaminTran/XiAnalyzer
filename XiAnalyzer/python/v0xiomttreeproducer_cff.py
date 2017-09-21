2a060efb5b7fc625cb82e70a86f3a5469cc014ab
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-09-21 03:14:20 +0200

    bugs

--- XiAnalyzer/python/v0xiomttreeproducer_cff.py
+++ XiAnalyzer/python/v0xiomttreeproducer_cff.py
@@ -4,27 +4,27 @@ from XiAnalyzer.XiAnalyzer.v0xiomttreeproducer_cfi import *
 
 KsTreeProducer = TreeProducer.clone(
         v0CollName = cms.string('generalV0CandidatesNew'),
-        v0IDName = cms.string('Kshort'),
-        multmax = cms.double(250)
+        v0IDName   = cms.string('Kshort'),
+        multmax    = cms.double(250)
         )
 
 LaTreeProducer = TreeProducer.clone(
-        v0CollName = cms.string('generalV0CandidatesNew'),
-        v0IDName = cms.string('Lambda'),
-        multmax = cms.double(250),
+        v0CollName   = cms.string('generalV0CandidatesNew'),
+        v0IDName     = cms.string('Lambda'),
+        multmax      = cms.double(250),
         misIDMassCut = cms.double(0.020)
         )
 
 XiTreeProducer = TreeProducer.clone(
         v0CollName = cms.string('generalCascadeCandidatesNew'),
-        v0IDName = cms.string('Xi'),
-        multmax = cms.double(250)
+        v0IDName   = cms.string('Xi'),
+        multmax    = cms.double(250)
         )
 
 OmTreeProducer = TreeProducer.clone(
-        v0CollName = cms.string('generalCascadeCandidatesNew'),
-        v0IDName = cms.string('Omega'),
-        multmax = cms.double(250),
+        v0CollName   = cms.string('generalCascadeCandidatesNew'),
+        v0IDName     = cms.string('Omega'),
+        multmax      = cms.double(250),
         misIDMassCut = cms.double(0.020)
         )
 
@@ -45,17 +45,17 @@ LaTreeProducerRapidity = LaTreeProducer.clone(
         )
 
 KsTreeProducerRapidityPbPb = KsTreeProducerRapidity.clone(
-        useCentrality cms.bool(True)
+        useCentrality = cms.bool(True)
         )
 
 LaTreeProducerRapidityPbPb = LaTreeProducerRapidity.clone(
-        useCentrality cms.bool(True)
+        useCentrality = cms.bool(True)
         )
 
 XiTreeProducerRapidityPbPb = XiTreeProducerRapidity.clone(
-        useCentrality cms.bool(True)
+        useCentrality = cms.bool(True)
         )
 
 OmTreeProducerRapidityPbPb = OmTreeProducerRapidity.clone(
-        useCentrality cms.bool(True)
+        useCentrality = cms.bool(True)
         )
