3e1febe0a8426c87a7a96d88b4d30e6d1a179c96
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-09-22 18:12:40 +0200

    initil commit

---
+++ XiAnalyzer/crab/v0_analyzer_old.py
@@ -0,0 +1,31 @@
+import os
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+#config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = os.path.expandvars('$CMSSW_BASE/src/XiAnalyzer/XiAnalyzer/test/v0analysis_cfg.py')
+#config.General.requestName = 'HLT185_250FlowCascadev22016PDCorrelationJL2'
+config.General.workArea = 'crab_dir/HLT185_250FlowCascadev22016PDRap'
+
+config.section_("Data")
+config.Data.inputDBS = 'phys03'
+config.Data.ignoreLocality = True
+#config.Data.primaryDataset = 'MinBias_TuneZ2star_7TeV_pythia6'
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.inputDataset = '/PAHighMultiplicity1/davidlw-RecoSkim2016_Pbp_V0Cascade_v1-97be9aa52ea60cba5455e64649c12464/USER'
+#config.Data.totalUnits = 300
+config.Data.unitsPerJob = 90000
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/btran/'
+config.Data.useParent = True
+config.Data.publication = False
+config.Data.publishDBS = 'phys03'
+config.Data.outputDatasetTag = 'RapidityCut_Cascade_Rereco_HM185_250Flow'
+
+config.section_("Site")
+config.Site.storageSite = 'T2_CH_CERN'
+#config.Site.whitelist = ['T2_US_MIT']
+config.Site.whitelist = ['T2_US_Vanderbilt']
