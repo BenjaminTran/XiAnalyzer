23a7f30d162bbf1ef0710c2f4e0ca5db6e09fe5f
Benjamin Tran <blt1@rice.edu>
2017-09-27 16:57:39 -0500

    fixed omega fit parameters

--- XiAnalyzer/python/xicorrelation_cff.py
+++ XiAnalyzer/python/xicorrelation_cff.py
@@ -27,10 +27,10 @@ omCorrelationRapidity = xiCorrelation.clone(
 
 xiCorrelationRapidity = xiCorrelation.clone(
         casCollection = cms.InputTag('selectV0CandidatesLowXiRapidity:Xi'),
-        ptBin         = cms.vdouble(1.0, 1.4, 1.8, 2.2, 2.8, 3.6, 4.6, 6.0, 7.2, 10, 20),
-        PtBinNum      = cms.int32(10),
-        xiMassMean    = cms.vdouble(1.32338 ,1.3227 ,1.32243 ,1.32235 ,1.32224 ,1.32218 ,1.32214 ,1.32211 ,1.32208 ,1.32236),
-        xiMassSigma   = cms.vdouble(0.00512355 ,0.00460879 ,0.0042615 ,0.0038439 ,0.00360748 ,0.00352663 ,0.00364015 ,0.00369722 ,0.00377408 ,0.00425549),
+        ptBin         = cms.vdouble(1.0, 1.4, 1.8, 2.2, 2.8, 3.6, 4.6, 6.0, 7.2, 10),
+        PtBinNum      = cms.int32(9),
+        xiMassMean    = cms.vdouble(1.32339 ,1.3227 ,1.32243 ,1.32234 ,1.32224 ,1.32218 ,1.32214 ,1.3221 ,1.32208),
+        xiMassSigma   = cms.vdouble(0.00546234 ,0.00477726 ,0.00445783 ,0.00400124 ,0.00382872 ,0.00379973 ,0.00392485 ,0.00394356 ,0.00484037),
         doRap = cms.bool(True)
         )
 
