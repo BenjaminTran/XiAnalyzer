013fbd4782e8ea991f3b4956e62fe98e869a3049
Benjamin Tran (LXPLUS) <blt1@rice.edu>
2017-09-29 20:02:44 +0200

    job submission but there is a bug in the v0 multicrab

--- XiAnalyzer/crab/multicrab3_v0_analyzer.py
+++ XiAnalyzer/crab/multicrab3_v0_analyzer.py
@@ -49,9 +49,13 @@ if __name__ == '__main__':
                 v0.config.Data.inputDataset = DataSet[num]
                 v0.config.General.workArea = 'crab_dir/HLT185_250FlowV0v2pbp2016PD' + str(num+1) + 'Rap'
                 v0.config.General.requestName = 'HLT185_250Flow2016CorrelationPbpV0PD' + str(num+1) + 'JL' + str(counter)
+            with open( 'V0VarStore.dat', 'w' ) as fle:
+                counter = counter + 1
+                fle.write( str(counter) )
+            submit(v0.config)
     if collID == 'pPbMB' or collID == 'PbpMB':
         if collID == 'pPbMB':
-            for num in range(0,1):
+            for num in range(1,2):
                 try:
                     with open( 'V0VarStore.dat', 'r' ) as fle:
                         counter = int( fle.readline() )
@@ -70,6 +74,10 @@ if __name__ == '__main__':
                 v0.config.Data.inputDataset = DataSet[num]
                 v0.config.General.workArea = 'crab_dir/HLT185_250FlowV0v2ppbMB2016PD' + str(num+1) + 'Rap'
                 v0.config.General.requestName = 'HLT185_250Flow2016CorrelationpPbMBV0PD' + str(num+1) + 'JL' + str(counter)
+                with open( 'V0VarStore.dat', 'w' ) as fle:
+                    counter = counter + 1
+                    fle.write( str(counter) )
+                submit(v0.config)
         else:
             for num in range(0,20):
                 try:
@@ -102,11 +110,11 @@ if __name__ == '__main__':
                 v0.config.Data.inputDataset = DataSet[num]
                 v0.config.General.workArea = 'crab_dir/HLT185_250FlowV0v2PbpMB2016PD' + str(num+1) + 'Rap'
                 v0.config.General.requestName = 'HLT185_250Flow2016CorrelationPbpMBV0PD' + str(num+1) + 'JL' + str(counter)
+                with open( 'V0VarStore.dat', 'w' ) as fle:
+                    counter = counter + 1
+                    fle.write( str(counter) )
+                submit(v0.config)
 
 
-    with open( 'V0VarStore.dat', 'w' ) as fle:
-        counter = counter + 1
-        fle.write( str(counter) )
-    submit(v0.config)
 
 
