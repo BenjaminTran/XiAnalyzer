1dfd7a188b43d58dd7eaa9203e0e5815f8d7c27c
Benjamin Tran <blt1@rice.edu>
2018-01-05 11:00:23 -0600

    Removed commented submodules

--- XiAnalyzer/python/massptproducer_cff.py
+++ XiAnalyzer/python/massptproducer_cff.py
@@ -2,32 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from XiAnalyzer.XiAnalyzer.massptproducer_cfi import *
 
-#XiMassPt = MassPt.clone(
-#    ks = cms.untracked.bool(False),
-#    la = cms.untracked.bool(False),
-#    om = cms.untracked.bool(False)
-#)
-#
-#OmMassPt = MassPt.clone(
-#        ks = cms.untracked.bool(False),
-#        la = cms.untracked.bool(False),
-#        xi = cms.untracked.bool(False)
-#        )
-#
-#KslaMassPt = MassPt.clone(
-#    xi = cms.untracked.bool(False)
-#)
-#
-#KsMassPt = MassPt.clone(
-#    xi = cms.untracked.bool(False),
-#    la = cms.untracked.bool(False)
-#)
-#
-#LaMassPt = MassPt.clone(
-#    xi = cms.untracked.bool(False),
-#    ks = cms.untracked.bool(False)
-#)
-
 MassPtRapidity = MassPt.clone(
         multHigh = cms.double(250),
         gnCollection = cms.InputTag('genParticles'),
